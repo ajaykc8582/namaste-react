592fd9e8077fe83ac15537932333ca11e6dde79b
Ajay Chaudhary <ajaychaudhary7980@gmail.com>
2023-09-18 19:26:32 +0530

    Food delivery app

--- App.js
+++ App.js
@@ -1,24 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import appLogo from "./assets/Hungarian2.png";
 
-//JSX - Html or XML like syntax
-const JsxHeading = ()=> (
-  <h1 id="heading" className="head" tabIndex="5">
-    Namaste React using JSX 🚀
-  </h1>
-);
+/** 
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - ResturantContainer
+ *      - RestaurantCard
+ *          - Image
+ *          - Name of resturant, Rating, cuisine, delivery time
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+*/
+
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={appLogo} />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = ()=>{
+    return(
+        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+            <img 
+            className="res-logo"
+            alt="res-logo"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/gku70ucmf5wuguk5c09e" />
+            <h3>Arsalan</h3>
+            <h5>4.4 star</h5>
+            <h5>24 MINS</h5>
+            <h5>Biryani, Mughlai, North Indian</h5>
+        </div>
+    )
+}
 
 
-//React functional component
-const HeadingComponent = () => (
- <div id="container">
-    {/* <JsxHeading /> */}
-    {JsxHeading()}
-  <h1 className="heading">This is functional component</h1>
-  <h2>{console.log("This is js")}</h2>
-</div>
-);
+const Body = () =>{
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+
+const AppLayout = () =>{
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout />);
