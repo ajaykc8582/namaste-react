9fc9c18626edcb9ced6bb2c6479bc5aa2e3fc102
Ajay Chaudhary <ajaychaudhary7980@gmail.com>
2023-09-19 17:10:14 +0530

    Multiple res card added

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ import appLogo from "./assets/Hungarian2.png";
  * - Contact
 */
 
-const Header = ()=>{
+const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
@@ -37,27 +37,32 @@ const Header = ()=>{
     )
 }
 
-const RestaurantCard = ()=>{
-    return(
-        <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
-            <img 
-            className="res-logo"
-            alt="res-logo"
-            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/gku70ucmf5wuguk5c09e" />
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+            <img
+                className="res-logo"
+                alt="res-logo"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/gku70ucmf5wuguk5c09e" />
             <h3>Arsalan</h3>
-            <h5>4.4 star</h5>
-            <h5>24 MINS</h5>
-            <h5>Biryani, Mughlai, North Indian</h5>
+            <h4>4.4 star</h4>
+            <h4>24 MINS</h4>
+            <h4>Biryani, Mughlai, North Indian</h4>
         </div>
     )
 }
 
 
-const Body = () =>{
+const Body = () => {
     return (
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
                 <RestaurantCard />
             </div>
         </div>
@@ -65,7 +70,7 @@ const Body = () =>{
 }
 
 
-const AppLayout = () =>{
+const AppLayout = () => {
     return (
         <div className="app">
             <Header />
