1ac91e04442688ad77c094540051640a6847bd49
Ajay Chaudhary <ajaychaudhary7980@gmail.com>
2023-10-21 21:29:05 +0530

    Fetching live data from swiggy

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 
 
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5775218&lng=88.4313413&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const json = await data.json();
+        // console.log(json);
+
+        setListOfRestaurants(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn"
+                    onClick={() => {
+                        const filteredList = listOfRestaurants.filter((res) =>
+                            res.info.avgRating > 4.4
+                        )
+                        setListOfRestaurants(filteredList);
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-container">
                 {
-                    resList.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
+                    listOfRestaurants.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
                 }
             </div>
         </div>
