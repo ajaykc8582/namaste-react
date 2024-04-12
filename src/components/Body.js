6256b5acdc63ba1fcee1ece139092df9e3a4703f
ajayQuad <156159630+ajayQuad@users.noreply.github.com>
2024-04-12 23:57:10 +0530

    RestaurantMenu Changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,111 +5,111 @@ import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 // import UserContext from "../utils/UserContext";
 
-
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState([]);
-    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-    const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
 
-    const HocRestaurantCardVeg = hocResCard(RestaurantCard);
+  const HocRestaurantCardVeg = hocResCard(RestaurantCard);
 
-    // console.log("body rendered", listOfRestaurants);
+  // console.log("body rendered", listOfRestaurants);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5775218&lng=88.4313413&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5775218&lng=88.4313413&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-        const json = await data.json();
-        console.log(json);
+    const json = await data.json();
+    // console.log("json", json);
 
-        setListOfRestaurants(
-            json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-        );
-        setFilteredRestaurant(
-            json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-        );
-    };
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-    const onlineStatus = useOnlineStatus();
+  const onlineStatus = useOnlineStatus();
 
-    if (onlineStatus === false)
-        return (
-            <h1>Looks like you're offline!! Please check your internet connection</h1>
-        );
+  if (onlineStatus === false)
+    return (
+      <h1>Looks like you're offline!! Please check your internet connection</h1>
+    );
 
-    // const {loggedInUser, setUserName } = useContext(UserContext);
+  // const {loggedInUser, setUserName } = useContext(UserContext);
 
-    // Conditional Rendering
-    return listOfRestaurants.length === 0 ? (
-        <Shimmer />
-    ) : (
-        <div className="body">
-            <div className="filter flex">
-                <div className="search m-4 p-4">
-                    <input
-                        type="text"
-                        className="border border-solid border-black"
-                        value={searchText}
-                        onChange={(e) => {
-                            setSearchText(e.target.value);
-                        }}
-                    />
-                    <button
-                        className="px-4 py-2 bg-green-100 m-4 rounded-lg"
-                        onClick={() => {
-                            const filteredRestaurant = listOfRestaurants.filter((res) =>
-                                res.info.name.toLowerCase().includes(searchText.toLowerCase())
-                            );
+  // Conditional Rendering
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
 
-                            setFilteredRestaurant(filteredRestaurant);
-                        }}
-                    >
-                        Search
-                    </button>
-                </div>
-                <div className="m-4 p-4 flex items-center">
-                    <button
-                        className="px-4 py-2 bg-gray-100 rounded-lg"
-                        onClick={() => {
-                            const filteredList = listOfRestaurants.filter(
-                                (res) => res.info.avgRating > 4.5
-                            );
-                            setListOfRestaurants(filteredList);
-                        }}
-                    >
-                        Top Rated Restaurants
-                    </button>
-                </div>
-                {/* <div className="m-4 p-4 flex items-center">
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.5
+              );
+
+              setFilteredRestaurant(filteredList); 
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+        {/* <div className="m-4 p-4 flex items-center">
                     <label>UserName : </label>
                    <input className="border border-black p-2" 
                    value={loggedInUser}
                    onChange={(e)=> setUserName(e.target.value)}
                    />
                 </div> */}
-            </div> 
-            <div className="flex flex-wrap">
-                {filteredRestaurant.map((restaurant) => (
-                    <Link
-                        to={"/restaurants/" + restaurant.info.id}
-                        key={restaurant.info.id}
-                    >
-                        {restaurant.info.veg === true ? (
-                            <HocRestaurantCardVeg resData={restaurant} />
-                        ) : (
-                            <RestaurantCard resData={restaurant} />
-                        )}
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
+      </div>
+      <div className="flex flex-wrap">
+        {filteredRestaurant.map((restaurant) => (
+          <Link 
+            to={"/restaurants/" + restaurant.info.id}
+            key={restaurant.info.id}
+          >
+            {restaurant.info.veg === true ? (
+              <HocRestaurantCardVeg resData={restaurant} />
+            ) : (
+              <RestaurantCard resData={restaurant} />
+            )}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
